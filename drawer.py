118f28016167f5759df7daa9391a57af63aaeeb7
Matuesz <bieganski.m@wp.pl>
2021-09-16 17:22:09 +0200

    lab2 - filling triangles finished, TODO shading rendering.

--- drawer.py
+++ drawer.py
@@ -1,49 +1,70 @@
 #!/usr/bin/env python3
 
 from typing import List, Tuple
-from profiler import Profiler
+import numpy as np
+
 # from drawer import bresenham
 
-def draw_pixels(pixels : List[Tuple[int, int]], w=640, h=480):
+def draw_pixels(pixels : List[Tuple[int, int]], w=640, h=480, color=0xff):
     from PIL import Image
     import matplotlib.pyplot as plt
     import numpy as np
 
     im = Image.new("L", (w, h))
     for p in pixels:
-        im.putpixel(p, 255)
+        im.putpixel(p, value=color)
     # im.show()
     plt.imshow(np.asarray(im))
     plt.show()
 
+def cross(v1, v2):
+    a = v1[1] * v2[2] - v1[2] * v2[1]
+    b = v1[2] * v2[0] - v1[0] * v2[2]
+    c = v1[0] * v2[1] - v1[1] * v2[0]
+    return a,b,c
 
+# * finds bounding box
+# * calculates barycentric coordinates
+# * draws pixel only if all it's barycentric coords are positive
 def fill_triangle(vs : List[Tuple[int, int]], w: int, h: int):
     assert len(vs) == 3
+    from itertools import product
 
-    # sort vertices descending by height
-    descending = lambda lst2 : sorted(lst2, key=lambda x : x[1], reverse=True)
-  
-    def getl2r2(top, mid, bot):
-        pass
-        l2 = bresenham(*bot, *top)
-        l2.reverse()
-        assert descending(l2) == l2
-        r2_1 = bresenham(*bot, *mid)
-        r2_1.reverse()
-        assert descending(r2_1) == r2_1
-        r2_2 = bresenham(*mid, *top)
-        r2_2.reverse()
-        assert descending(r2_2) == r2_2
-        r2 = r2_1 + r2_2
-        assert len(l2) == len(r2), f"{len(l2)}xx{len(r2)}"
+    ws, hs = zip(*vs)
+    bbox_top, bbox_bot = max(hs), min(hs)
+    bbox_r, bbox_l = max(ws), min(ws)
 
-        while len(l2):
-            yield l2.pop(), r2.pop()
+    points = list(product(
+        range(bbox_l, bbox_r + 1),
+        range(bbox_bot, bbox_top + 1),
+    ))
+    assert len(points) == (bbox_top - bbox_bot + 1) * (bbox_r - bbox_l + 1)
+    
+    points = np.array(points)
+    vs = np.array(vs) # for vectorized + and - ops
+    
+    # TODO
+    # why the hell I had to use BA and CA instead of AC, CA to make it work?
+    a = vs[0]
+    ab = a - vs[1]
+    ac = a - vs[2]
 
-    fill = []
-    for l2, r2 in getl2r2(*descending(vs)):
-        fill.extend(bresenham(*l2, *r2))
-    draw_pixels(fill, w, h)
+    pixels = []
+    for p in points:
+        pa = p - a
+        v1, v2 = np.array([ac, ab, pa]).T.astype(float)
+        x, y, z = np.cross(v1, v2).astype(float)
+        if z == 0:
+            continue
+        barycentric = np.array([
+            1.0 - (x + y) / z, 
+            x / z,
+            y / z
+        ])
+        if all(barycentric >= 0):
+            pixels.append(p)
+    return pixels     
+    # draw_pixels(pixels, w, h, color=color)
 
 
 def bresenham(x0, y0, x1, y1) -> Tuple[int, int]:
--- solutions.py
+++ solutions.py
@@ -1,6 +1,15 @@
+from numpy.lib.twodim_base import tri
 from model_parser import ModelParser
-from pathlib import Path
 from drawer import draw_pixels, bresenham, fill_triangle
+
+from pathlib import Path
+from operator import itemgetter
+from itertools import combinations, chain
+
+from PIL import Image
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
 path = Path("model.obj")
 
@@ -13,9 +22,6 @@ class Solution():
 
         print(f"len vertices = {len(vs)}")
         print(f"len faces = {len(fs)}")
-
-        from operator import itemgetter
-        from itertools import combinations, chain
 
         w, h = 400, 400
         pixels = []
@@ -36,4 +42,49 @@ class Solution():
         vs = ModelParser.get_vertices(path)
         fs = ModelParser.get_faces(path)
 
-        fill_triangle([(3,4), (100,50), (300, 300)], 400, 400)
+        def calculate_color(triangle):
+
+            # https://stackoverflow.com/a/40360416
+            def normalize(v):
+                norm=np.linalg.norm(v, ord=2)
+                if norm==0:
+                    norm=np.finfo(float).eps
+                return v/norm
+            triangle = [list(x) for x in triangle]
+            # for p in triangle:
+            #     p.append(0)
+            a, b, c = map(np.array, triangle)
+            # raise ValueError(a, b, c)
+            ab, ac = b - a, c - a
+            # raise ValueError(ab, ac)
+            v = np.cross(ab, ac)
+            v = normalize(v)
+            norm=np.linalg.norm(v, ord=2)
+            print("n: ", norm(v))
+            # print(v)
+            light = np.array([1,1,1])
+            intensity = np.dot(light, v)
+            if intensity <= 0:
+                print("ZLE")
+                return 0
+            # else:
+            #     print("OK")
+            # print(intensity)
+            res = int(255 * intensity)
+            return res
+            
+        # tu jestem
+        # okazuje się że do policzenia light trzeba(na pewno?) koordy floatowe. TODO why
+        w, h = 400, 400
+        im = Image.new("L", (w, h))
+        for f in tqdm(fs):
+            fcoords3 = itemgetter(*f)(vs)
+            # raise ValueError(fcoords3)
+            # we don't care about Z axis for now.
+            triangle = list(map(lambda abc: (int((abc[0] + 1.0) * w/2) - 1, int((abc[1] + 1.0) * h/2) - 1), fcoords3))
+            color = calculate_color(fcoords3)
+            pixels = fill_triangle(triangle, w, h)
+            for p in pixels:
+                im.putpixel(p, value=color)
+        plt.imshow(np.asarray(im))
+        plt.show()
